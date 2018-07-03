55b14177edd8c172be64b602ce6f24c810bd3a8f
zisehuiyi2007 <rosiwang@163.com>
2018-07-03 10:43:12 +0800

    add video

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <img src="./assets/logo.png">
     <p>{{message}}</p>
+    <div v-on:click="playVideo()">
+      <video :src="videoSrc" id="videoPlay" controls="controls" width="700"
+             v-show="true" class="video">您的浏览器不支持 video 视屏播放。</video>
+    </div>
     <router-link to="/home">Home</router-link>
     <router-link to="/vueTest">TestTest</router-link>
     <router-view/>
@@ -9,40 +13,47 @@
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data:function(){
-    return {
-      message:"测试测试"
-          }
-  },
-  created(){
-    this.defaultPath();
-  },
-  watch:{
-    "$route"(to){
+  // import router from
+  export default {
+    name: 'App',
+    data:function(){
+      return {
+        message:"测试测试",
+        videoSrc: 'static/video/test.mp4'     //获取视频播放资源
+      }
+    },
+    created(){
       this.defaultPath();
-    }
-  },
-  methods:{
-    defaultPath(){
-      if(this.$route.fullPath=='/'){
-        console.log('path00:',this.$route.path);
-        router.push("/home");
-        console.log('path:',this.$route.path);
+    },
+    watch:{
+      "$route"(to){
+        this.defaultPath();
+      }
+    },
+    methods:{
+      defaultPath(){
+        // if(this.$route.fullPath=='/'){
+        //   console.log('path00:',this.$route.path);
+        //   router.push("/home");
+        //   console.log('path:',this.$route.path);
+        // }
+      },
+      playVideo(){
+        var vdo = document.getElementById("videoPlay");
+        vdo.play();
       }
     }
+
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
